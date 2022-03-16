0d269efcf6b5e14bf7f221c474cb9f9d0f443228
haresh musti <hareshkm999@gmail.com>
2022-03-16 14:02:46 +0530

    modfied app

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from wsgiref import simple_server
 from flask import Flask, request, render_template
 from flask import Response
 
-port = int(os.getenv("PORT", 5000))
+app = Flask(__name__)
+
+@app.route("/train", methods=['GET', 'POST'])
+#@cross_origin()
+def trainRouteClient():
+    return "hi"
+
+
 if __name__ == "__main__":
     host = '0.0.0.0'
-    # port = 5000
+    port = 5000
     httpd = simple_server.make_server(host, port, app)
     # print("Serving on %s %d" % (host, port))
-	print("hi")
+    print("hi")
     httpd.serve_forever()
-
-print("hi")
