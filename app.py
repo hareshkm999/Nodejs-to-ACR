216cf5b9cf2dc4937e4bece173f6b4a60e5016b6
haresh musti <hareshkm999@gmail.com>
2022-03-17 09:22:21 +0530

    github actions deployment workflow

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ from flask import Response
 
 app = Flask(__name__)
 
-@app.route("/train", methods=['GET', 'POST'])
-#@cross_origin()
-def trainRouteClient():
-    return "hi"
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return {"data": "Application ran successfully - FastAPI"}
 
 
 if __name__ == "__main__":
     host = '0.0.0.0'
-    port = 5000
+    port = 8800
     httpd = simple_server.make_server(host, port, app)
     # print("Serving on %s %d" % (host, port))
     print("hi")
